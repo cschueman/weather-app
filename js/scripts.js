51d01e7c040970e07a9aa03947a307bc8440074d
cschueman <cschueman@eagles.ewu.edu>
2017-12-05 08:34:15 -0800

    Getting started
    Added simple weather in js and scss
    Got things copied over from code pen

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,49 @@
-// Randomize Header
+$.simpleWeather({
+    location: 99203,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('.city').text(weather.city);
+      $('.temp').text(weather.temp);
+      $('.thumbnail').attr('src', weather.image);
+      // icon
+      var iconCode = 'wi wi-yahoo-' + weather.code;
+      $('i').attr('class', iconCode );
+      
+      // Conditions
+      // if ( weather.code >= 26 && weather.code <= 30  ) {
+      //   $('body').addClass('cloudy');    
+      // }
+      
+      // Forecast
+      $('#forecast tr:nth-child(1) .day').text(weather.forecast[1].day);
+      $('#forecast tr:nth-child(2) .day').text(weather.forecast[2].day);
+      $('#forecast tr:nth-child(3) .day').text(weather.forecast[3].day);
+      
+      $('#forecast tr:nth-child(1) .high').text(weather.forecast[1].high);
+      $('#forecast tr:nth-child(2) .high').text(weather.forecast[2].high);
+      $('#forecast tr:nth-child(3) .high').text(weather.forecast[3].high);
+      
+      $('#forecast tr:nth-child(1) .low').text(weather.forecast[1].low);
+      $('#forecast tr:nth-child(2) .low').text(weather.forecast[2].low);
+      $('#forecast tr:nth-child(3) .low').text(weather.forecast[3].low);
+      
+      // Icon
+      var dayOneIcon = 'wi wi-yahoo-' + weather.forecast[1].code;
+      var dayTwoIcon = 'wi wi-yahoo-' + weather.forecast[2].code;
+      var dayThreeIcon = 'wi wi-yahoo-' + weather.forecast[3].code;
+        
+      $('#forecast tr:nth-child(1) i').attr('class', dayOneIcon );
+      $('#forecast tr:nth-child(2) i').attr('class', dayTwoIcon );
+      $('#forecast tr:nth-child(3) i').attr('class', dayThreeIcon );
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
+  
+  });
 
-// Store & Generate Number
-var maxRand = 3;
-var numRand = Math.floor( Math.random() * maxRand );
-//console.log(numRand);
-
-// Store images
-var img0 = 'http://placehold.it/800x400';
-var img1 = 'http://placehold.it/800x400';
-var img2 = 'http://placehold.it/800x400';
-
-// Show Images
-$('header img').attr('src', eval('img' + numRand) );
-
-// Activate Gallery
-$('figure').lightGallery({
-    selector: 'a', 
-    thumbnail: true,
-});
-
-$('figure').lightGallery({
-  selector: 'a',
-  mode: 'lg-fade',
-  download: false,
-  speed: 100,
-  share: false,
-  autoplay: false,
-});
