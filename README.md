256f749f8b4f56699aba43d650d57552025127f7
cschueman <cschueman@eagles.ewu.edu>
2017-12-05 08:22:58 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# stand-alone-portfolio
+Stand Alone Portfolio
+
+## content priority outline
+it's thing I made
+
+## code samples
+
+## writing
+
+## photos
+
+## logos
---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// Randomize Header
+
+// Store & Generate Number
+var maxRand = 3;
+var numRand = Math.floor( Math.random() * maxRand );
+//console.log(numRand);
+
+// Store images
+var img0 = 'http://placehold.it/800x400';
+var img1 = 'http://placehold.it/800x400';
+var img2 = 'http://placehold.it/800x400';
+
+// Show Images
+$('header img').attr('src', eval('img' + numRand) );
+
+// Activate Gallery
+$('figure').lightGallery({
+    selector: 'a', 
+    thumbnail: true,
+});
+
+$('figure').lightGallery({
+  selector: 'a',
+  mode: 'lg-fade',
+  download: false,
+  speed: 100,
+  share: false,
+  autoplay: false,
+});
