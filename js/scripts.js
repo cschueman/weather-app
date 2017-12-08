9d881a33cc34e3193c1f6782c8a31ffded859c4a
cschueman <cschueman@eagles.ewu.edu>
2017-12-07 22:07:47 -0800

    so i changed my mind
    now it's a sheep pun generator fight me

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,75 @@
-$.simpleWeather({
-    location: 'Cheney, WA',
+// Get Geo Location
+// Check for GeoLocation Support on Browser
+if ('geolocation' in navigator) {
+
+   $('.geo').show(); 
+
+} else {
+  
+  $('.geo').hide();
+  $('.geo').prepend('<p>Geolocation Not Supported</p>');
+
+}
+
+// On Click, Get Geolocation, Call Weather Function
+$('.geo').click( function() {
+      
+    //load weather using your lat/long coordinates
+    navigator.geolocation.getCurrentPosition(function(position) {
+      
+      // Check lat/long coordinates
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      
+      console.log(lat, long);
+      
+      // Send to SimpleWeather
+      getWeather( lat + ',' + long ); 
+    });
+   
+});
+
+// Show geolocated weather
+var getWeather = function(location) {
+    
+   $.simpleWeather({
+    location: location,
     unit: 'f',
     success: function(weather) {
+      
       // Entire weather object
       console.log(weather);
       
       // Display Data
-      $('.city').text(weather.city);
       $('.temp').text(weather.temp);
-      $('.thumbnail').attr('src', weather.image);
-      // icon
-      var iconCode = 'wi wi-yahoo-' + weather.code;
-      $('i').attr('class', iconCode );
-      
-      // Conditions
-      // if ( weather.code >= 20 && weather.code <= 30  ) {
-      //   $('body').addClass('cloudy');    
-      // }
-      
-      // Forecast
-      $('#forecast tr:nth-child(1) .day').text(weather.forecast[1].day);
-      $('#forecast tr:nth-child(2) .day').text(weather.forecast[2].day);
-      $('#forecast tr:nth-child(3) .day').text(weather.forecast[3].day);
-       $('#forecast tr:nth-child(4) .day').text(weather.forecast[4].day);
-       $('#forecast tr:nth-child(5) .day').text(weather.forecast[5].day);
-      
-      $('#forecast tr:nth-child(1) .high').text(weather.forecast[1].high);
-      $('#forecast tr:nth-child(2) .high').text(weather.forecast[2].high);
-      $('#forecast tr:nth-child(3) .high').text(weather.forecast[3].high);
-      $('#forecast tr:nth-child(4) .high').text(weather.forecast[4].high);
-      $('#forecast tr:nth-child(5) .high').text(weather.forecast[5].high);
-      
-      $('#forecast tr:nth-child(1) .low').text(weather.forecast[1].low);
-      $('#forecast tr:nth-child(2) .low').text(weather.forecast[2].low);
-      $('#forecast tr:nth-child(3) .low').text(weather.forecast[3].low);
-      $('#forecast tr:nth-child(4) .low').text(weather.forecast[4].low);
-      $('#forecast tr:nth-child(5) .low').text(weather.forecast[5].low);
-      
-      // Icon
-      var dayOneIcon = 'wi wi-yahoo-' + weather.forecast[1].code;
-      var dayTwoIcon = 'wi wi-yahoo-' + weather.forecast[2].code;
-      var dayThreeIcon = 'wi wi-yahoo-' + weather.forecast[3].code;
-      var dayFourIcon = 'wi wi-yahoo-' + weather.forecast[4].code;
-      var dayFiveIcon = 'wi wi-yahoo-' + weather.forecast[5].code;
+      $('.city').text(weather.city);
+      $('img').attr('src', weather.image);
         
-      $('#forecast tr:nth-child(1) i').attr('class', dayOneIcon );
-      $('#forecast tr:nth-child(2) i').attr('class', dayTwoIcon );
-      $('#forecast tr:nth-child(3) i').attr('class', dayThreeIcon );
-   $('#forecast tr:nth-child(4) i').attr('class', dayFourIcon );
-    $('#forecast tr:nth-child(5) i').attr('class', dayFiveIcon );
+
     },
-                
     error: function(error) {
       // Show if weather cannot be retreived
-      console.log('Look outside.');
+      console.log('Look Outside.');
     }
   
   });
+    
+};
+
+// 1. page load 2. store headers 3. ... x. Show header
+
+// Store headers
+var pun0 = 'Whats a sheeps favorite computer component? The RAM!';
+var pun1 = 'Why did the sheep baaa? Because thats what noise they make';
+var pun2 = 'What is a sheeps favorite newspaper? The Wool Street Journal';
+var pun3 = 'Holy Sheep';
+var pun4 = 'Ewe are so silly';
+
+// Store & Generate Number
+var maxRand = 5;
+var randNum = Math.floor( Math.random() * maxRand );
+console.log(randNum);
+
+//  Show headers
+$('p').text( eval('pun' + randNum) );
 
