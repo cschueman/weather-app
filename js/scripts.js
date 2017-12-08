afa2b37ba40d7d4ff8b94ecbccb03a8b81d031fb
cschueman <cschueman@eagles.ewu.edu>
2017-12-07 18:04:31 -0800

    Added favicons
    also made it a five day forecast

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $.simpleWeather({
-    location: 99203,
+    location: 'Cheney, WA',
     unit: 'f',
     success: function(weather) {
       // Entire weather object
@@ -14,7 +14,7 @@ $.simpleWeather({
       $('i').attr('class', iconCode );
       
       // Conditions
-      // if ( weather.code >= 26 && weather.code <= 30  ) {
+      // if ( weather.code >= 20 && weather.code <= 30  ) {
       //   $('body').addClass('cloudy');    
       // }
       
@@ -22,24 +22,35 @@ $.simpleWeather({
       $('#forecast tr:nth-child(1) .day').text(weather.forecast[1].day);
       $('#forecast tr:nth-child(2) .day').text(weather.forecast[2].day);
       $('#forecast tr:nth-child(3) .day').text(weather.forecast[3].day);
+       $('#forecast tr:nth-child(4) .day').text(weather.forecast[4].day);
+       $('#forecast tr:nth-child(5) .day').text(weather.forecast[5].day);
       
       $('#forecast tr:nth-child(1) .high').text(weather.forecast[1].high);
       $('#forecast tr:nth-child(2) .high').text(weather.forecast[2].high);
       $('#forecast tr:nth-child(3) .high').text(weather.forecast[3].high);
+      $('#forecast tr:nth-child(4) .high').text(weather.forecast[4].high);
+      $('#forecast tr:nth-child(5) .high').text(weather.forecast[5].high);
       
       $('#forecast tr:nth-child(1) .low').text(weather.forecast[1].low);
       $('#forecast tr:nth-child(2) .low').text(weather.forecast[2].low);
       $('#forecast tr:nth-child(3) .low').text(weather.forecast[3].low);
+      $('#forecast tr:nth-child(4) .low').text(weather.forecast[4].low);
+      $('#forecast tr:nth-child(5) .low').text(weather.forecast[5].low);
       
       // Icon
       var dayOneIcon = 'wi wi-yahoo-' + weather.forecast[1].code;
       var dayTwoIcon = 'wi wi-yahoo-' + weather.forecast[2].code;
       var dayThreeIcon = 'wi wi-yahoo-' + weather.forecast[3].code;
+      var dayFourIcon = 'wi wi-yahoo-' + weather.forecast[4].code;
+      var dayFiveIcon = 'wi wi-yahoo-' + weather.forecast[5].code;
         
       $('#forecast tr:nth-child(1) i').attr('class', dayOneIcon );
       $('#forecast tr:nth-child(2) i').attr('class', dayTwoIcon );
       $('#forecast tr:nth-child(3) i').attr('class', dayThreeIcon );
+   $('#forecast tr:nth-child(4) i').attr('class', dayFourIcon );
+    $('#forecast tr:nth-child(5) i').attr('class', dayFiveIcon );
     },
+                
     error: function(error) {
       // Show if weather cannot be retreived
       console.log('Look outside.');
